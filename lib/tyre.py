311ed6eeabc8fc6ba8b941894152d160c0168ba9
Chris Adams <cn10cra@gmailcom>
2026-04-22 15:31:38 +0100

    tyre to use dict

--- lib/tyre.py
+++ lib/tyre.py
@@ -1,15 +1,21 @@
 class Tyre():
     def __init__(self):
-        self.tread_depth = None
-        self.pressure = None
-        self.date_recorded = None
+    
         self.history = []
-
+        self.tyre_info = {
+            "tread_depth": None,
+            "pressure": None,
+            "date_recorded": None
+        }
     def record_tyre_data(self, tread_depth, pressure, date_recorded):
-        self.tread_depth = tread_depth
-        self.pressure = pressure
-        self.date_recorded = date_recorded
+        self.tyre_info = {
+            "tread_depth": tread_depth,
+            "pressure": pressure,
+            "date_recorded": date_recorded
+        }
+
+        self.history.append(self.tyre_info)
+
 
     def get_history(self):
-        
         return self.history
